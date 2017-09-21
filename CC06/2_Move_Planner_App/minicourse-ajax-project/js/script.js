6f873647351728ba5dcd8abcb371a37e5bdb21cb
anujaverma11 <anuja.verma@gmail.com>
2017-09-21 03:52:58 -0700

    Move Planner App

--- CC06/2_Move_Planner_App/minicourse-ajax-project/js/script.js
+++ CC06/2_Move_Planner_App/minicourse-ajax-project/js/script.js
@@ -12,10 +12,47 @@ function loadData() {
     $nytElem.text("");
 
     // load streetview
+    var streetStr = ('#street').val();
+    var cityStr = ('#city').val();
+    var address = streetStr +', '+ cityStr;
+    console.log(address);
+    $greeting.text('So you want to live at' + address + '?');
 
-    // YOUR CODE GOES HERE!
+    var streetviewUrl = 'https://maps.googleapis.com/maps/api/streetview?size=600x400&location=' +address +'';
+    // $body.append('<img class="bgimg" src="'+ streetviewUrl +'">')
+    $body.append('<img class="bgimg" src="https://static.pexels.com/photos/126407/pexels-photo-126407.jpeg">')
 
-    return false;
+
+    // return false;
 };
 
-$('#form-container').submit(loadData);
+
+$('#form-container').submit(loadData);});
+
+// function loadData() {
+
+//     var $body = $('body');
+//     var $wikiElem = $('#wikipedia-links');
+//     var $nytHeaderElem = $('#nytimes-header');
+//     var $nytElem = $('#nytimes-articles');
+//     var $greeting = $('#greeting');
+//     var streetInput = $('#street').val();
+//     var cityInput = $('#city').val();
+//     var bgLocation = streetInput + ', ' + cityInput;
+//     var streetViewURL = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + bgLocation + '';
+
+//     // clear out old data before new request
+//     $wikiElem.text("");
+//     $nytElem.text("");
+
+//     $greeting.text("Is " + bgLocation + " where you want to live?")
+
+//     // load streetview
+//     $body.append('<img class="bgimg" src="' + streetViewURL + '">');
+
+//     return false;
+// };
+
+// $('#form-container').submit(loadData);
+
+// // loadData();
